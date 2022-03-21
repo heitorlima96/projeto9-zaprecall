c6384ce636028813c08d5b18d966d4ed757f63a6
Heitor Lima <limaheitor.1996@gmail.com>
2022-03-21 16:01:56 -0300

    Projeto

---
+++ src/components/Conteudo/components/Topo.jsx
@@ -0,0 +1,10 @@
+import Logo from "./../../../assets/logo.png"
+
+export default function Topo() {
+  return (
+    <div className="topo">
+      <img src={Logo} alt="Logo ZapRecall" />
+      <h2>ZapRecall</h2>
+    </div>
+  )
+}
